bf25adb54d6f1a299c6910c95ea685babd1cf518
thaobaan <thaobaan@gmail.com>
2023-10-11 02:49:41 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Button from "./Button"
+import Nav from "./Nav"
+import ShoeCard from "./ShoeCard"
+import PopularProductCard from "./PopularProdcutCard"
+import ReviewCard from "./ReviewCard"
+import ServiceCard from "./ServiceCard"
+
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+    ReviewCard,
+    ServiceCard
+}
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+
+
+export {
+    Hero, 
+    PopularProducts,
+    SuperQuality,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    CustomerReview,
+    Footer
+}
